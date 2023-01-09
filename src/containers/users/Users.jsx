1c4ac9ca3e12f86fe66efcdb520a15930f71bbce
Gray <blackcat5523@gmail.com>
2023-01-09 14:33:15 +0800

    使用tailwind套件

--- src/containers/users/Users.jsx
+++ src/containers/users/Users.jsx
@@ -2,19 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./users.css";
 
-import { HeatImage } from "./HeatImage";
-import { TitleButtons } from "./TitleButtons";
+
 const Users = () => (
     <div id='users'>
-        <HeatImage />
-        <h1>您好 May 你現在是藝拍小夥伴啦！</h1>
-        <TitleButtons
-         icon='?' 
-         word='帳務設定' 
-         />
+        
+        <h1 className="text-center">您好 MAY 你現在是藝拍小夥伴啦！</h1>
+        
 
-        {/* <br />
-    <button className='login-button'><Link to='/users/login' >Login</Link></button> */}
+        <br />
+    <button className='login-button'><Link to='/users/login' >Login</Link></button>
     </div>
 );
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/containers/users/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
